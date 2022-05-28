aa28fbd21c0b33ecfe7f193071a7ceaf170bcc2e
Dzyfhuba <hafidz21ub@gmail.com>
2022-05-28 12:30:33 +0700

    update image style on article page

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,10 +5,10 @@
     <div class="container mt-3">
         <div class="row justify-content-between">
             @foreach ($posts as $key => $post)
-                <div class="p-3 {{ $key % 3 ? ' col-6' : ' col-12' }}">
+                <div class="p-3 {{ $key % 3 ? ' col-lg-6' : ' col-lg-12' }}">
                     <article id="article"
                         class="row d-flex align-items-center bg-white border border-1 border-light shadow p-3"
-                        style="height: 200px">
+                        style="min-height: 200px">
                         {{ preg_match_all('/\((.*?)\)/', $post->description, $result) ? '' : '' }}
                         <textarea class="d-none md">{{ $post->description }}</textarea>
                         <a href="{{ route('article.show', Str::slug($post->title)) }}"
@@ -17,10 +17,10 @@
                         </a>
                         <img style="cursor: pointer"
                             onclick="window.location.href='{{ route('article.show', Str::slug($post->title)) }}'"
-                            class="img-fluid col-2 lazyload{{ $key % 3 ? ' p-0' : '' }}"
+                            class="col-lg-2 col-4 lazyload{{ $key % 3 ? ' img-half-width' : ' img-full-width' }}"
                             src="{{ asset('images/spinner.gif') }}" data-src="{{ $result[1][0] }}"
                             alt="{{ Str::slug($post->title) }}">
-                        <div class="content col-10"></div>
+                        <div class="content col-lg-10 col-8"></div>
                     </article>
                 </div>
             @endforeach
